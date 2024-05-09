412830e5a9f40fae92c424375ceb7b0bfd8d7f1a
RoyerL01 <royemlugsan16@gmail.com>
2024-05-08 21:17:02 -0600

    Codigo Individual Rogelio

---
+++ flutter-roll_dice-main/flutter-roll_dice-main/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:custom_app_test/gradient_container.dart';
+
+void main() {
+  runApp(MaterialApp( 
+    home: Scaffold(
+      body: GradientContainer(
+          [Color.fromARGB(255, 255, 47, 144), Color.fromARGB(255, 33, 247, 254)]),
+    ),
+  ));
+}
